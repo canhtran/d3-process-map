c4ad4be0e213d229377b2aebf41d170ac058f5b9
canhtran <trandcanh@gmail.com>
2018-10-25 18:38:35 +0800

    update function get data

--- web/util/__init__.py
+++ web/util/__init__.py
@@ -5,6 +5,11 @@ def read_data(data_url):
 	with open(data_url) as json_data:
 		json = json.load(json_data)
 
-	data = []
-	errors = []
+	data = {}
+	errors = {}
+
+	for obj in json:
+		obj['dependedOnBy'] = []
+		data[obj['name']] = obj
+
 
