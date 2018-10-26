f6da55467d46306bedcb6c5a86a4f8d948d11823
canhtran <trandcanh@gmail.com>
2018-10-26 18:18:25 +0800

    fix NoneType object

--- web/util/__init__.py
+++ web/util/__init__.py
@@ -2,21 +2,22 @@ import json
 
 
 def read_data(data_url):
-    data = {}
-    errors = []
-    with open(data_url) as json_data:
-        json_data = json.load(json_data)
-    for obj in json_data:
-        obj['dependedOnBy'] = []
-        data[obj['name']] = obj
-        for name in obj['depends']:
-            if name in data:
-                data[name]['dependedOnBy'].append(obj['name'])
-            else:
-                errors = errors.append(
-                    "Unrecognized dependency: %s depends on %s" % (obj[name], name)
-                )
-    return data
+	data = {}
+	errors = []
+	with open(data_url) as json_data:
+		json_data = json.load(json_data)
+	for obj in json_data:
+		obj['dependedOnBy'] = []
+		data[obj['name']] = obj
+		for name in obj['depends']:
+			if name in data:
+				data[name]['dependedOnBy'] = []
+				data[name]['dependedOnBy'].append(obj['name'])
+			else:
+				errors.append(
+					"Unrecognized dependency: %s depends on %s" % (obj['name'], name)
+				)
+	return data
 
 
 def read_config(config_url, data_url):
